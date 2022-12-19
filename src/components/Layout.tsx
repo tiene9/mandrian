32225852ff2595033f34392d42c3ac6e82cb7fb0
EtienneActual <etienne.brun@groupeactual.eu>
2022-12-19 23:09:48 +0100

    mondrian grid

---
+++ src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import "../styles/main.scss";
+
+const Layout = () => {
+  return (
+    <div className="main">
+      <div className="first row">
+        <div className="left-case" />
+        <div className="right-case" />
+      </div>
+      <div className="black-bar" />
+      <div className="second row">
+        <div className="left-case" />
+        <div className="right-case" />
+      </div>
+      <div className="black-bar" />
+
+      <div className="third row">
+        <div className="left-case" />
+        <div className="right-case" />
+      </div>
+      <div className="black-bar" />
+
+      <div className="fourth">
+        <div className="full-line" />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
